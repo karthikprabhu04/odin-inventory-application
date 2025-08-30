c42b99cabfc49d436d58137831945891cabc0d04
karthikprabhu04 <kpp32@cam.ac.uk>
2025-08-30 14:54:48 +0100

    Changed background colour

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // Routes
 app.use("/", usersRouter);
